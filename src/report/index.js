6e3e43977db2606c599821f3b9d9a6b62efb978b
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2021-03-29 01:43:44 +0200

    fix: reporting

--- src/report/index.js
+++ src/report/index.js
@@ -14,175 +14,68 @@ const requireJson = (resultsPath, filename) => {
   }
 };
 
-const smallUrl = (url) =>
-  url
-    .toLowerCase()
-    .replace(/^https?:\/\/?/, "")
-    .replace(/\/$/, "");
-
-const isSameUrl = (url1, url2) => smallUrl(url1) === smallUrl(url2);
-
-const isSameHost = (url1, url2) => toHostname(url1) === toHostname(url2);
-
-const owaspCleanup = (result) => {
-  return {
-    ...result,
-    site: result.site.map((site) => {
+const zapCleanup = (result) => ({
+  ...result,
+  site:
+    result.site &&
+    result.site.map((site) => {
       return {
         ...site,
-        alerts: site.alerts.map(
-          ({ name, riskcode, confidence, riskdesc, desc, ...others }) => {
-            return {
-              name,
-              riskcode,
-              confidence,
-              riskdesc,
-              desc,
-            };
-          }
+        alerts: site.alerts.map((result) =>
+          pick(result, ["name", "riskcode", "confidence", "riskdesc", "desc"])
         ),
       };
     }),
-  };
-};
+});
 
-const exportData = async (resultsPath) => {
-  // aggregate results
-  const allData = {
-    http: requireJson(resultsPath, "http.json"),
-    ssl: requireJson(resultsPath, "ssl.json"),
-    trackers: requireJson(resultsPath, "trackers.json"),
-    nuclei: requireJson(resultsPath, "nuclei.json"),
-    geoip: requireJson(resultsPath, "geoip.json"),
-    lhr: await getScansFiles(
-      resultsPath,
-      /^lhr-.*\.json$/,
-      (node) => node.requestedUrl
-    ),
-    owasp: await getScansFiles(
-      resultsPath,
-      /^zap-.*\.json$/,
-      (node) => node.site[0]["@host"]
-    ),
-  };
-
-  // group by url, remove some data
-  const byUrl = getUrls().reduce((a, currentUrl) => {
+const cleanups = {
+  nuclei: (result) => result.map((r) => omit(r, ["request", "response"])),
+  zap: zapCleanup,
+  lhr: (result) => {
+    const { requestedUrl, finalUrl, categories, audits } = result;
+    // strip some data
+    const newCategories = Object.keys(categories).reduce(
+      (a, key) => ({
+        ...a,
+        [key]: omit(categories[key], "auditRefs"),
+      }),
+      {}
+    );
     return {
-      ...a,
-      [currentUrl]: {
-        http: allData.http
-          .filter((result) => isSameUrl(currentUrl, result.url))
-          .map((result) => ({
-            ...omit(result.result, "responseHeaders"),
-          })),
-        ssl: allData.ssl
-          .filter((result) => isSameUrl(currentUrl, result.url))
-          .map((result) => ({
-            ...result.result,
-          })),
-        trackers: allData.trackers.filter((result) =>
-          isSameUrl(currentUrl, result.url)
-        ),
-        nuclei: allData.nuclei
-          .filter((result) => isSameUrl(currentUrl, result.host))
-          .map(({ request, response, ...data }) => data), // strip some data
-        lhr: allData.lhr
-          .filter((result) => isSameUrl(currentUrl, result.url))
-          .map(({ url, filename, result }) => {
-            const { requestedUrl, finalUrl, categories, audits } = result;
-            // strip some data
-            const newCategories = Object.keys(categories).reduce(
-              (a, key) => ({
-                ...a,
-                [key]: omit(categories[key], "auditRefs"),
-              }),
-              {}
-            );
-            return {
-              url,
-              filename,
-              requestedUrl,
-              finalUrl,
-              categories: newCategories,
-              audits: pick(audits, ["metrics", "diagnostics"]),
-            };
-          }),
-        owasp: allData.owasp
-          .filter((result) => isSameHost(currentUrl, result.url))
-          .map((result) => ({
-            filename: result.filename,
-            ...owaspCleanup(result.result),
-          })),
-        geoip: allData.geoip.filter((result) =>
-          isSameUrl(currentUrl, result.url)
-        ),
-      },
+      requestedUrl,
+      finalUrl,
+      categories: newCategories,
+      audits: pick(audits, ["metrics", "diagnostics"]),
     };
-  }, {});
-
-  return byUrl;
-};
-
-const getScansFiles = (resultsPath, pattern, getUrl) => {
-  const files = fs.readdirSync(resultsPath);
-  return files
-    .filter((filename) => filename.match(pattern))
-    .map((filename) => {
-      const data = requireJson(resultsPath, filename);
-      const url = getUrl(data);
-      return {
-        url,
-        filename,
-        result: data,
-      };
-    });
+  },
 };
 
 const generateReport = () => {
   const urls = getUrls().map((url) => {
-    console.log(url);
     const urlb64 = Buffer.from(url).toString("base64");
-    const urlPath = path.join(__dirname, "..", "..", "results", urlb64);
+    const urlPath = path.join("results", urlb64);
     const scans = fs.readdirSync(urlPath);
     scans.sort().reverse();
     const lastScan = scans.length && scans[0];
     if (!lastScan) {
       return null;
     }
-    console.log("lastScan", lastScan);
     const latestFilesPath = path.join(urlPath, lastScan);
     const latestFiles = fs.readdirSync(latestFilesPath);
-    //const files = fs.readdirSync(path.join(urlPath, lastScan));
-    //console.log("files", files);
     return {
       url,
       http: requireJson(latestFilesPath, "http.json"),
       ssl: requireJson(latestFilesPath, "ssl.json"),
       thirdparties: requireJson(latestFilesPath, "thirdparties.json"),
-      zap: requireJson(latestFilesPath, "zap.json"),
-      nuclei: requireJson(latestFilesPath, "nuclei.json"),
-      lhr: requireJson(latestFilesPath, "lhr.json"),
-
-      // nuclei: requireJson(resultsPath, "nuclei.json"),
-      // geoip: requireJson(resultsPath, "geoip.json"),
-      // lhr: await getScansFiles(
-      //   resultsPath,
-      //   /^lhr-.*\.json$/,
-      //   (node) => node.requestedUrl
-      // ),
-      // owasp: await getScansFiles(
-      //   resultsPath,
-      //   /^zap-.*\.json$/,
-      //   (node) => node.site[0]["@host"]
-      // ),
+      zap: cleanups.zap(requireJson(latestFilesPath, "zap.json")),
+      nuclei: cleanups.nuclei(requireJson(latestFilesPath, "nuclei.json")),
+      lhr: cleanups.lhr(requireJson(latestFilesPath, "lhr.json")),
     };
   });
   return urls;
 };
 
 if (require.main === module) {
-  generateReport();
-  //.then((d) => console.log(JSON.stringify(d, null, 2)))
-  //.catch(console.log);
+  const report = generateReport();
+  console.log(JSON.stringify(report, null, 2));
 }
